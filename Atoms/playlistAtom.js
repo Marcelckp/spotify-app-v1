7caa0d607c38e4793552fdf9b7c7dded9a8a50c6
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-28 23:11:52 +0200

    created atom to store play list state and playlist name

---
+++ Atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const playlistAtom = atom({
+    key: 'playlist',
+    default: null
+})
+
+export const playlistStateAtom = atom({
+    key: 'playlistState',
+    default: null
+})
