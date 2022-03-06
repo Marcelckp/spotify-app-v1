3e54dc3378b5179f824b688c11c0e89f608df97e
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-03-06 18:43:45 +0200

    fixed typescript getServerSideProps typing issue & modularized the default functional component function

--- pages/likes.tsx
+++ pages/likes.tsx
@@ -2,8 +2,9 @@ import Head from "next/head";
 import LikePanel from "../components/LikePanel";
 import { AppContext } from "next/app";
 import { getSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
 
-function Like() {
+export default function Like() {
   return (
     <div className="bg-gradient-to-b from-black via-black to-[#111] h-screen overflow-hidden w-full">
       <Head>
@@ -14,9 +15,7 @@ function Like() {
   );
 }
 
-export default Like;
-
-export const getServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
 
   return {
